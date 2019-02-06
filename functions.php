4af847b1d6983fe04db8088c3633bb081d37baed
Rose Cox <rose@purpleprodigy.com>
2019-02-06 12:54:37 +1000

    Remove woocommerce support

--- functions.php
+++ functions.php
@@ -212,13 +212,6 @@ function rose_scripts_styles() {
 	// Enqueue custom Google fonts.
 	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Montserrat+Sans:400,600,700', array(), CHILD_THEME_VERSION );
 
-	// Conditionally load WooCommerce styles.
-	if ( rose_is_woocommerce_page() ) {
-
-		wp_enqueue_style( CHILD_TEXT_DOMAIN . '-woocommerce', CHILD_THEME_URI . '/woocommerce.css', array(), CHILD_THEME_VERSION );
-
-	}
-
 	// Conditionally load slider scripts.
 	if ( ! class_exists( 'SEO_Slider_Widget' ) ) {
 
